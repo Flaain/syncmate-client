b5f58ebcc0798ca46e7d019d0168830cdea34b69
Ruslan Salmanov <flainof@gmail.com>
2024-10-13 17:49:15 +0300

    fix: type in OutletDetails

--- src/widgets/OutletDetails/model/constants.ts
+++ src/widgets/OutletDetails/model/constants.ts
@@ -1,6 +1,6 @@
 import { FeedTypes } from '@/shared/model/types';
 
-export const titles: Record<Exclude<FeedTypes, 'User'>, string> = {
+export const titles: Record<Exclude<FeedTypes, 'User' | 'ADS'>, string> = {
     Conversation: 'User Info',
     Group: 'Group Info'
 };
