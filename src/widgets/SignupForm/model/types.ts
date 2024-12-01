0bbca868b88d46cc55f620ef9811d7e2ff07c623
Ruslan Salmanov <flainof@gmail.com>
2024-12-01 15:34:16 +0300

    fix: fixed typing

--- src/widgets/SignupForm/model/types.ts
+++ src/widgets/SignupForm/model/types.ts
@@ -1,14 +1,8 @@
 import { z } from "zod";
 import { signupSchema } from "./schema";
 import { UseFormReturn } from "react-hook-form";
-import { APIData, BasicAPIResponse, Profile, UserCheckParams } from "@/shared/model/types";
 
 export type SignupSchemaType = z.infer<typeof signupSchema>;
-
-export interface ISignupAPI {
-    signup: (body: Omit<SignupSchemaType, 'confirmPassword'> & { otp: string }) => Promise<APIData<Profile>>;
-    check: (body: UserCheckParams) => Promise<APIData<BasicAPIResponse>>;
-}
 
 export interface ISignupContext {
     step: number;
