3fb5c7d2a3e960b60baab27370255d0bdd26e0ad
Ruslan Salmanov <flainof@gmail.com>
2025-03-29 22:39:49 +0300

    feat: back old vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src')
-        }
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-    server: {
-        host: true,
-        port: 4000,
-        watch: {
-            usePolling: true
-        }
-    }
 });
