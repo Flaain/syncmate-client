c2a19ae7ee2fd0063bbe34a69a0e8c28989c0726
Ruslan Salmanov <flainof@gmail.com>
2025-04-29 01:44:52 +0300

    feat: standardize name length constants and update related error messages

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -20,14 +20,15 @@ export const localStorageKeys = {
 
 export const MIN_USER_SEARCH_LENGTH = 2;
 export const MESSAGES_SKELETON_COUNT = 12;
-export const MAX_NAME_LENGTH = 32;
+export const NAME_MAX_LENGTH = 32;
+export const BIO_MAX_LENGTH = 120;
 export const MAX_POINTER_DISTANCE_DDM = 200;
 
 export const ONLY_LATIN_REGEXP = /^[a-zA-Z0-9_]*$/;
 export const ALLOW_CYRILLIC_REGEXP = /^[\p{L}0-9\s]*$/u;
 
 export const REGEXP_NAME_ERROR = 'Name must contain only letters, numbers, and spaces';
-export const NAME_TO_LONG_ERROR = `Name must be at most ${MAX_NAME_LENGTH} characters long`;
+export const NAME_TO_LONG_ERROR = `Name must be at most ${NAME_MAX_LENGTH} characters long`;
 
 export const emailForSchema = z.string().trim().min(1, 'Email is required').email('Invalid email address').toLowerCase();
 export const passwordForSchema = z
@@ -37,7 +38,7 @@ export const passwordForSchema = z
     .min(6, 'Password must be at least 6 characters long')
     .max(32, 'Password must be at most 32 characters long');
 
-export const nameForSchema = z.string().trim().min(1, 'Name is required').max(MAX_NAME_LENGTH, NAME_TO_LONG_ERROR);
+export const nameForSchema = z.string().trim().min(1, 'Name is required').max(NAME_MAX_LENGTH, NAME_TO_LONG_ERROR);
 export const nameSchema = z.object({ name: nameForSchema });
 
 export const loginForSchema = z
