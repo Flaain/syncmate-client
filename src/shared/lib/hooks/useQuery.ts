4bf0f57d488da8581de2b2f26cd66358c7923d13
Ruslan Salmanov <flainof@gmail.com>
2025-05-02 00:06:59 +0300

    feat: enhance useQuery hook with cache handling and error management improvements

--- src/shared/lib/hooks/useQuery.ts
+++ src/shared/lib/hooks/useQuery.ts
@@ -2,13 +2,16 @@ import React from 'react';
 
 import { ApiBaseResult } from '@/shared/api/API';
 import { ApiException } from '@/shared/api/error';
+import { QueryCache } from '@/shared/model/queryCache';
 
 export type UseQueryCallback<T> = (params: { signal: AbortSignal }) => Promise<ApiBaseResult<T>>;
 
 type UseQueryReducerAction =
     | { type: UseQueryTypes.LOADING; payload: { isLoading: boolean } }
+    | { type: UseQueryTypes.UPDAITING; payload: { isUpdating: boolean; } }
     | { type: UseQueryTypes.SUCCESS }
-    | { type: UseQueryTypes.ERROR; payload: { error: ApiException } }
+    | { type: UseQueryTypes.CACHE_SUCCESS }
+    | { type: UseQueryTypes.ERROR; payload: { error: unknown } }
     | { type: UseQueryTypes.REFETCH; payload: { isRefetching: true } }
     | { type: UseQueryTypes.RESET; payload: { isLoading: false, isRefetching: false } }
 
@@ -16,11 +19,13 @@ type UseRunQueryAction = 'init' | 'refetch';
 
 enum UseQueryTypes {
     LOADING = 'loading',
+    UPDAITING = 'updating',
     SUCCESS = 'success',
     SET = 'set',
     REFETCH = 'refetch',
     RESET = 'reset',
     ERROR = 'error',
+    CACHE_SUCCESS = 'cache_success'
 }
 
 interface UseQueryOptions<T> {
@@ -29,6 +34,8 @@ interface UseQueryOptions<T> {
     enabled: boolean;
     refetchInterval: number;
     retryDelay: number;
+    prefix: string;
+    shouldUseCache: boolean;
     initialData: (() => T) | T;
     cleanup: () => void;
     onSuccess: (data: T) => void;
@@ -48,11 +55,13 @@ interface UseQueryConfig {
 
 interface UseQueryReturn<T> {
     isLoading: boolean;
+    isUpdating: boolean;
     isRefetching: boolean;
     isSuccess: boolean; 
-    isError: boolean; 
+    isCacheSuccess: boolean;
+    isError: boolean;
     data: T; 
-    error?: ApiException | undefined;
+    error?: unknown;
     call: () => Promise<void>;
     abort: () => void;
     setData: React.Dispatch<React.SetStateAction<T>>;
@@ -64,24 +73,27 @@ interface UseQueryReducerState {
     isSuccess: boolean;
     isError: boolean;
     isRefetching: boolean;
-    error?: ApiException;
+    isUpdating: boolean;
+    isCacheSuccess: boolean;
+    error?: unknown;
 }
 
-const errorAction = (error: ApiException): Extract<UseQueryReducerAction, { type: UseQueryTypes.ERROR }> => ({
-    type: UseQueryTypes.ERROR,
-    payload: { error }
-});
-
-const queryReducer = (state: UseQueryReducerState, action: UseQueryReducerAction) => {
+const queryReducer = (state: UseQueryReducerState, action: UseQueryReducerAction): UseQueryReducerState => {
     switch (action.type) {
         case UseQueryTypes.LOADING:
             return { ...state, isLoading: action.payload.isLoading };
+        case UseQueryTypes.UPDAITING:
+            return { ...state, isUpdating: action.payload.isUpdating };
+        case UseQueryTypes.CACHE_SUCCESS:
+            return { ...state, isUpdating: false, isCacheSuccess: true, isLoading: false }
         case UseQueryTypes.SUCCESS:
             return {
                 ...state,
+                isUpdating: false,
                 isSuccess: true,
                 isLoading: false,
                 isRefetching: false,
+                isCacheSuccess: false,
                 isError: false,
                 error: undefined
             };
@@ -94,7 +106,7 @@ const queryReducer = (state: UseQueryReducerState, action: UseQueryReducerAction
                 isError: true,
                 isSuccess: false,
                 isRefetching: false,
-                isLoading: false
+                isLoading: state.isUpdating && !state.isCacheSuccess
             };
         case UseQueryTypes.RESET:
             return { ...state, ...action.payload };
@@ -108,12 +120,16 @@ const actions: Record<UseRunQueryAction, UseQueryReducerAction> = {
     refetch: { type: UseQueryTypes.REFETCH, payload: { isRefetching: true } }
 };
 
+const queryCache = new QueryCache();
+
 export const useQuery = <T>(callback: UseQueryCallback<T>, options?: Partial<UseQueryOptions<T>>): UseQueryReturn<T> => {
     const [state, dispatch] = React.useReducer(queryReducer, {
         isError: false,
         isLoading: options?.enabled ?? true,
         isSuccess: false,
         isRefetching: false,
+        isCacheSuccess: false,
+        isUpdating: false,
         error: undefined
     });
 
@@ -129,22 +145,54 @@ export const useQuery = <T>(callback: UseQueryCallback<T>, options?: Partial<Use
         timeout: null 
     });
 
-    const abort = React.useCallback(() => {
+    const abort = () => {
         config.current.abortController.abort();
         config.current.abortController = new AbortController();
-    }, []);
+
+        config.current.interval && clearInterval(config.current.interval);
+    };
     
+    const runCache = async (retires: number = Number(options?.retry) || 0) => {
+        try {
+            dispatch({ type: UseQueryTypes.UPDAITING, payload: { isUpdating: true } });
+
+            const cache = await (await queryCache.open()).match(options!.prefix!);
+
+            if (!cache) return;
+            
+            const data = await cache.json();
+
+            if (!state.isSuccess) {
+                dispatch({ type: UseQueryTypes.CACHE_SUCCESS });
+                console.log('cache success', data)
+                setData(options?.onSelect?.(data) ?? data);
+
+                options?.onSuccess?.(data);
+            }
+        } catch (error) {
+            if (retires > 0 && !state.isSuccess) return runCache(retires - 1);
+
+            console.error(error);
+
+            dispatch({ type: UseQueryTypes.UPDAITING, payload: { isUpdating: false } });
+        }
+    };
+
     const runQuery = async (action: UseRunQueryAction) => {
         try {
             abort();
 
             config.current.currentAction !== action && (dispatch(actions[action]), (config.current.currentAction = action));
 
-            const { data } = await callback({ signal: config.current.abortController.signal });
+            const { data, authentic } = await callback({ signal: config.current.abortController.signal });
+
+            const selectedData = options?.onSelect?.(data) ?? data;
 
             dispatch({ type: UseQueryTypes.SUCCESS });
 
-            setData(options?.onSelect?.(data) ?? data);
+            setData(selectedData);
+
+            options?.prefix && queryCache.open().then((cache) => cache?.put(options.prefix!, authentic));
 
             options?.onSuccess?.(data);
 
@@ -152,34 +200,55 @@ export const useQuery = <T>(callback: UseQueryCallback<T>, options?: Partial<Use
 
             options?.refetchInterval && (config.current.interval = setInterval(runQuery, options.refetchInterval, 'refetch'));
         } catch (error) {
+            if (error instanceof DOMException && error.name === 'AbortError') {
+                console.log('[useQuery] Request aborted.');
+                return;
+            }
+
             if (error instanceof ApiException) {
+                console.error('[useQuery] API Error:', error);
+
+                if (error.response.status === 401) return;
+
                 if (config.current.retry > 0) {
                     config.current.retry -= 1;
-                    
+
+                    console.log(`[useQuery] Retrying request... (${config.current.retry} attempts left)`);
+
                     const timeout = setTimeout(() => {
                         runQuery(action);
                         clearTimeout(timeout);
                     }, options?.retryDelay ?? 1000);
-                    
+
                     config.current.timeout = timeout;
                 } else {
-                    dispatch(errorAction(error));
+                    dispatch({ type: UseQueryTypes.ERROR, payload: { error } });
 
+                    config.current.requested = false;
                     config.current.currentAction = null;
                     config.current.retry = Number(options?.retry) || 0;
                 }
-                
+
                 config.current.interval && clearInterval(config.current.interval);
+
+                options?.onError?.(error);
+            } else {
+                console.error('[useQuery] Unknown Error:', error);
+
+                dispatch({ type: UseQueryTypes.ERROR, payload: { error: error } });
 
                 options?.onError?.(error);
             }
         }
-    };
+    }
 
     React.useEffect(() => {
         config.current.mounted = true;
+        
+        if (!(options?.enabled ?? true)) return;
 
-        (options?.enabled ?? true) && runQuery('init');
+        runQuery('init');
+        options?.prefix && runCache();
 
         return () => {
             abort();
