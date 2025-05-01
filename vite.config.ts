0e8388cab378b8077d1f672173cb0770ac3e9ac5
Ruslan Salmanov <flainof@gmail.com>
2025-05-02 00:02:08 +0300

    feat: restructure vite.config.ts to enhance PWA configuration and manifest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,47 @@
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
+const manifest: Partial<ManifestOptions> = {
+    name: 'Syncmate',
+    short_name: 'Syncmate',
+    icons: [
+        {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+        },
+        {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+        }
+    ],
+    theme_color: '#1E1E1E',
+    background_color: '#1E1E1E',
+    display: 'standalone'
+};
+
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{html,css,js,ico,png,svg}']
+            },
+            manifest: manifest
+        })
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
         }
-    },
+    }
 });
