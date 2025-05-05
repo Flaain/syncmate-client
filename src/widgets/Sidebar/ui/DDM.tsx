54474debfae764c252884a9e787076a43f26ab75
Ruslan Salmanov <flainof@gmail.com>
2025-05-05 17:50:02 +0300

    refactor: update DDM component to improve avatar rendering and add version display

--- src/widgets/Sidebar/ui/DDM.tsx
+++ src/widgets/Sidebar/ui/DDM.tsx
@@ -3,12 +3,14 @@ import { useShallow } from 'zustand/shallow';
 
 import { useProfile } from '@/entities/profile';
 
+import { APP_VERSION } from '@/shared/constants';
 import { AvatarByName } from '@/shared/ui/AvatarByName';
 import { Button } from '@/shared/ui/button';
 import { DDM as DropdownMenu } from '@/shared/ui/DDM';
 import { DropdownMenuSeparator } from '@/shared/ui/dropdown-menu';
 import { Image } from '@/shared/ui/Image';
 import { MenuItem } from '@/shared/ui/MenuItem';
+import { Typography } from '@/shared/ui/Typography';
 
 import { SidebarMenus } from './ui';
 
@@ -31,15 +33,15 @@ export const DDM = ({ changeMenu }: { changeMenu: (menu: SidebarMenus | null) =>
                 type='ddm'
                 className='gap-4'
                 onClick={() => changeMenu('settings')}
-                icon={
-                    <Image
-                        className='size-6 rounded-full border-[1.5px] border-solid border-primary-blue object-cover'
-                        src={avatar?.url}
-                        skeleton={<AvatarByName name={name} className='size-5 text-sm' />}
-                    />
-                }
                 text={name}
-            />
+                displayChildrenFrom='left'
+            >
+                <Image
+                    className='size-6 rounded-full border-[1.5px] border-solid border-primary-blue object-cover'
+                    src={avatar?.url}
+                    skeleton={<AvatarByName name={name} className='size-5 text-sm' />}
+                />
+            </MenuItem>
             <DropdownMenuSeparator className='dark:bg-primary-dark-50' />
             <MenuItem
                 className='gap-4'
@@ -57,6 +59,9 @@ export const DDM = ({ changeMenu }: { changeMenu: (menu: SidebarMenus | null) =>
                 text='Settings'
                 onClick={() => changeMenu('settings')}
             />
+            <Typography as='p' variant='secondary' size='xs' className='flex flex-col mt-auto px-4 py-1'>
+                Syncmate, Version {APP_VERSION}
+            </Typography>
         </DropdownMenu>
     );
 };
