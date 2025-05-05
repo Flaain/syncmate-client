b5e50b492442ffb08f5e9470ebc5c213e8f0536a
Ruslan Salmanov <flainof@gmail.com>
2025-05-05 17:47:35 +0300

    refactor: remove change password and edit name features from the codebase

--- src/features/change-password/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { api, ApiBaseSuccessData } from '@/shared/api';
-
-import { UserPasswordParams } from '../model/types';
-
-export const changePasswordAPI = {
-    changePassword: ({ type, ...body }: UserPasswordParams) => api.post<ApiBaseSuccessData>(`/auth/password`, body, { params: { type } })
-};
--- src/features/change-password/model/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-import { passwordForSchema } from "@/shared/constants";
-
-export const changePasswordSchema = z.object({
-    currentPassword: passwordForSchema,
-    newPassword: passwordForSchema
-})
