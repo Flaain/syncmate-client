1665a219dcca7c73bd6e828b4a95353be4406fa0
Ruslan Salmanov <flainof@gmail.com>
2025-05-05 17:48:20 +0300

    refactor: update ProfileMenuSkeleton to use concise arrow function syntax and ensure consistent skeleton rendering

--- src/features/sidebar-menus/model/view.ts
+++ src/features/sidebar-menus/model/view.ts
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import { DataStorageMenuSkeleton } from '../ui/data/Skeleton';
+import { ProfileMenuSkeleton } from '../ui/profile/ProfileMenuSkeleton';
 import { SettingsMenuSkeleton } from '../ui/settings/Skeleton';
 
 export const SettingsContent = React.lazy(() => import('../ui/settings/Content').then((module) => ({ default: module.SettingsContent })).catch(() => ({ default: SettingsMenuSkeleton })));
-export const ProfileContent = React.lazy(() => import('../ui/profile/Content').then((module) => ({ default: module.ProfileContent })).catch(() => ({ default: SettingsMenuSkeleton })));
+export const ProfileContent = React.lazy(() => import('../ui/profile/Content').then((module) => ({ default: module.ProfileContent })).catch(() => ({ default: ProfileMenuSkeleton })));
+export const DataStorageContent = React.lazy(() => import('../ui/data/Content').then((module) => ({ default: module.DataStorageMenuContent })).catch(() => ({ default: DataStorageMenuSkeleton })));
