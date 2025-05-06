fbe68b1ce85d40c540002ee6d0a5714852a8421f
Ruslan Salmanov <flainof@gmail.com>
2025-05-06 21:51:08 +0300

    feat: replace Lucide icons with SVG counterparts in DDM and Sidebar for improved consistency and performance

--- src/widgets/Sidebar/ui/DDM.tsx
+++ src/widgets/Sidebar/ui/DDM.tsx
@@ -1,7 +1,12 @@
-import { AlignJustifyIcon, Archive, BookmarkIcon, Settings, Video } from 'lucide-react';
 import { useShallow } from 'zustand/shallow';
 
 import { useProfile } from '@/entities/profile';
+
+import ArchiveIcon from '@/shared/lib/assets/icons/archive.svg?react';
+import MenuIcon from '@/shared/lib/assets/icons/menu.svg?react';
+import SavedMessagesIcon from '@/shared/lib/assets/icons/savedmessages.svg?react';
+import SettingsIcon from '@/shared/lib/assets/icons/settings.svg?react';
+import StoriesIcon from '@/shared/lib/assets/icons/stories.svg?react';
 
 import { APP_VERSION } from '@/shared/constants';
 import { AvatarByName } from '@/shared/ui/AvatarByName';
@@ -14,18 +19,18 @@ import { Typography } from '@/shared/ui/Typography';
 
 import { SidebarMenus } from './ui';
 
-const iconStyles = 'size-4 mx-1';
+const iconStyles = 'size-5 mx-1';
 
 export const DDM = ({ changeMenu }: { changeMenu: (menu: SidebarMenus | null) => void }) => {
     const { name, avatar } = useProfile(useShallow((state) => ({ name: state.profile.name, avatar: state.profile.avatar })));
 
     return (
         <DropdownMenu
-            className='w-[180px]'
+            className='min-w-[200px]'
             collisionPadding={{ left: 12 }}
             trigger={
                 <Button variant='ghost' size='icon' className='rounded-full p-2 opacity-50'>
-                    <AlignJustifyIcon />
+                    <MenuIcon className='text-white' />
                 </Button>
             }
         >
@@ -46,20 +51,20 @@ export const DDM = ({ changeMenu }: { changeMenu: (menu: SidebarMenus | null) =>
             <MenuItem
                 className='gap-4'
                 type='ddm'
-                icon={<BookmarkIcon className={iconStyles} />}
+                icon={<SavedMessagesIcon className={iconStyles} />}
                 text='Saved Messages'
             />
-            <MenuItem className='gap-4' type='ddm' icon={<Archive className={iconStyles} />} text='Archived Chats' />
-            <MenuItem className='gap-4' type='ddm' icon={<Video className={iconStyles} />} text='My Stories' />
+            <MenuItem className='gap-4' type='ddm' icon={<ArchiveIcon className={iconStyles} />} text='Archived Chats' />
+            <MenuItem className='gap-4' type='ddm' icon={<StoriesIcon className={iconStyles} />} text='My Stories' />
             <DropdownMenuSeparator className='dark:bg-primary-dark-50' />
             <MenuItem
                 className='gap-4'
                 type='ddm'
-                icon={<Settings className={iconStyles} />}
+                icon={<SettingsIcon className={iconStyles} />}
                 text='Settings'
                 onClick={() => changeMenu('settings')}
             />
-            <Typography as='p' variant='secondary' size='xs' className='flex flex-col mt-auto px-4 py-1'>
+            <Typography as='p' variant='secondary' size='xs' className='flex flex-col text-center py-1'>
                 Syncmate, Version {APP_VERSION}
             </Typography>
         </DropdownMenu>
--- src/widgets/Sidebar/ui/ui.tsx
+++ src/widgets/Sidebar/ui/ui.tsx
@@ -1,7 +1,9 @@
-import { Loader2, X } from 'lucide-react';
-
 import { Feed } from '@/features/feed';
 import { SettingsMenu } from '@/features/sidebar-menus';
+
+import CloseIcon from '@/shared/lib/assets/icons/close.svg?react';
+import LoaderIcon from '@/shared/lib/assets/icons/loader.svg?react';
+import SearchIcon from '@/shared/lib/assets/icons/search.svg?react';
 
 import { useSidebarMenu } from '@/shared/lib/hooks/useSidebarMenu';
 import { cn } from '@/shared/lib/utils/cn';
@@ -31,14 +33,15 @@ export const Sidebar = () => {
     return (
         <aside className='grid grid-cols-1 h-dvh sticky top-0 overflow-hidden gap-2 max-md:fixed dark:bg-primary-dark-150 bg-primary-white md:max-w-[420px] w-full md:border-r-2 md:border-r-primary-dark-50 md:border-solid'>
             <div ref={panelRef} className={cn('flex flex-col col-start-1 row-start-1 duration-300 overflow-hidden z-0', activeMenu && '-translate-x-20')}>
-                <div className='flex items-center justify-between gap-5 sticky top-0 p-4 box-border h-[70px]'>
+                <div className='flex items-center justify-between gap-3 sticky top-0 px-4 box-border h-[56px] mb-3'>
                     <DDM changeMenu={setActiveMenu} />
                     <div className='flex w-full relative'>
                         {isDisconnected && (
                             <div className='absolute left-3 top-1/2 -translate-y-1/2'>
-                                <Loader2 className='animate-spin size-5 z-10 text-primary-white' />
+                                <LoaderIcon className='animate-loading size-6 z-10 text-primary-gray pointer-events-none' />
                             </div>
                         )}
+                        {!isDisconnected && <SearchIcon className='absolute pointer-events-none top-1/2 -translate-y-1/2 text-primary-gray size-6 left-3' />}
                         <Input
                             _size='sm'
                             variant='dark'
@@ -47,11 +50,11 @@ export const Sidebar = () => {
                             onChange={handleSearch}
                             value={value}
                             placeholder={!connectedToNetwork ? 'Waiting for network' : !isSocketConnected ? 'Connecting...' : 'Search...'}
-                            className={cn('flex-1 pr-9', isDisconnected && 'pl-10')}
+                            className='flex-1 px-12 py-0 rounded-full'
                         />
                     {!!value.trim().length && (
-                        <Button variant='text' size='icon' onClick={resetSearch} className='p-0 absolute right-2 top-1/2 -translate-y-1/2'>
-                            <X className='w-5 h-5' />
+                        <Button variant='text' size='icon' onClick={resetSearch} className='p-0 absolute right-3 top-1/2 -translate-y-1/2'>
+                            <CloseIcon className='size-6 text-primary-gray' />
                         </Button>
                     )}
                     </div>
