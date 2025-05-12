15dad9d610ccb5fb9a5a7d6944b8001a86d4c3c3
Flaain <flainof@gmail.com>
2025-05-13 02:04:37 +0300

    feat: delete src/entities/Message directory

--- src/entities/Message/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './api'
-export * from './ui/ui'
-
-export { endpoints } from './model/constants'
--- src/entities/Message/model/constants.ts
+++
@@ -1,7 +0,0 @@
-import { CHAT_TYPE } from "@/shared/model/types";
-
-export const endpoints: Record<CHAT_TYPE, string> = {
-    Conversation: 'message',
-};
-
-export const ctxMenuIconStyles = 'size-5'
