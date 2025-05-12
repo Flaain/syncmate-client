067d2feec85caa1ab572cc4a65018ab8adb85057
Flaain <flainof@gmail.com>
2025-05-13 02:03:27 +0300

    feat: delete src/features/Feed directory

--- src/features/Feed/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ui/ui";
-export * from './api';
-
-export { type GlobalResults } from './model/types';
--- src/features/Feed/utils/getSortedFeed.ts
+++
@@ -1,5 +0,0 @@
-import { LocalFeed } from "../model/types";
-
-export const getSortedFeedByLastMessage = (a: LocalFeed, b: LocalFeed) => {
-    return new Date(b.lastActionAt).getTime() - new Date(a.lastActionAt).getTime();
-};
