86dc07bd93801f270804e7f578474a61b6686036
Flaain <flainof@gmail.com>
2025-05-13 02:03:41 +0300

    feat: delete src/features/Forgot directory

--- src/features/Forgot/api/index.ts
+++
@@ -1,5 +0,0 @@
-import { api, ApiBaseSuccessData } from '@/shared/api';
-
-export const forgotAPI = {
-    reset: (body: { email: string; otp: string; password: string }) => api.post<ApiBaseSuccessData>('/auth/reset', body)
-};
