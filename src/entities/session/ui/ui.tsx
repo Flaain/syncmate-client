63357c0b5fe4bb0fa5e085a88dc2a43661d5a1df
Ruslan Salmanov <flainof@gmail.com>
2025-06-03 20:14:02 +0300

    refactor: simplify Session component structure for better readability

--- src/entities/session/ui/ui.tsx
+++ src/entities/session/ui/ui.tsx
@@ -8,28 +8,22 @@ export const Session = ({ session, isCurrent }: SessionProps) => {
     const createdAt = new Date(session.createdAt);
 
     return (
-        <div className='flex items-start gap-5 flex-1'>
-            <div className='flex items-center flex-1'>
-                <div className='flex flex-col items-start'>
-                    <Typography as='h3'>
-                        {userAgent?.browser.name ?? 'Unknown browser'}&nbsp;{userAgent?.browser?.major}
-                    </Typography>
-                    <Typography as='h3' size='sm'>
-                        {userAgent?.os.name ?? 'Unknown OS'}
-                    </Typography>
-                </div>
-                {!isCurrent && (
-                    <Typography
-                        as='p'
-                        title={`Created at ${createdAt.toLocaleString()}`}
-                        variant='secondary'
-                        className='ml-auto line-clamp-1'
-                        size='sm'
-                    >
-                        {new Intl.DateTimeFormat('en-EN', { day: '2-digit', month: 'short' }).format(createdAt)}
-                    </Typography>
-                )}
-            </div>
-        </div>
+        <>
+            <Typography className='text-left'>
+                {userAgent?.browser.name ?? 'Unknown browser'}&nbsp;{userAgent?.browser?.major}
+                <br />
+                {userAgent?.os.name ?? 'Unknown OS'}
+            </Typography>
+            {!isCurrent && (
+                <Typography
+                    title={`Created at ${createdAt.toLocaleString()}`}
+                    variant='secondary'
+                    className='ml-auto line-clamp-1'
+                    size='sm'
+                >
+                    {new Intl.DateTimeFormat('en-EN', { day: '2-digit', month: 'short' }).format(createdAt)}
+                </Typography>
+            )}
+        </>
     );
 };
