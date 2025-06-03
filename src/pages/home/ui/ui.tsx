90e4582eb8eaf738cb9668d0d4863e42be8de85e
Ruslan Salmanov <flainof@gmail.com>
2025-06-03 20:16:47 +0300

    refactor: update layout and enhance styling in Home component

--- src/pages/home/ui/ui.tsx
+++ src/pages/home/ui/ui.tsx
@@ -1,14 +1,16 @@
 import ChatsPlaceholderIcon from '@/shared/lib/assets/icons/chatsplaceholder.svg?react';
 
+import { Pattern } from '@/shared/ui/Pattern';
 import { Typography } from '@/shared/ui/Typography';
 
 export const Home = () => {
     return (
-        <div className='max-md:hidden flex flex-col flex-1 gap-5 items-center justify-center dark:bg-primary-dark-200 bg-primary-white px-2'>
-            <Typography as='h1' variant='primary' size='2xl' weight='bold' align='center' className='max-w-[400px]'>
+        <div className='max-lg:hidden flex flex-col flex-1 gap-5 items-center justify-center dark:bg-primary-dark-200 bg-primary-white px-2 relative'>
+            <Pattern />
+            <Typography as='h1' variant='primary' size='2xl' weight='bold' align='center' className='max-w-[400px] z-10'>
                 Select a chat to start messaging or create one
             </Typography>
-            <ChatsPlaceholderIcon className='text-primary-white size-32' />
+            <ChatsPlaceholderIcon className='text-primary-white size-32 z-10' />
         </div>
     );
 };
